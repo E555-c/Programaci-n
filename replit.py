0af6cfe7abd73ed53c592017b48bdf3852180400
E555-c <evepacheco419@gmail.com>
2020-03-03 02:26:05 +0000

    Hello Winsconsin

---
+++ replit.py
@@ -0,0 +1,10 @@
+#El Hola Mundo es muy conocido en Python, por eso decidi cambiar la frase
+#Aunque no altera en nada lo que queremos decir ya que como quiera le estoy dando la orden de que lo imprima
+#Asi hubiera puesto otra frase diferente seria igual 
+
+#El def lo utilizamos para definir una funcion y al terminar el cogigo tenemos que mandarla a llamar para que haga lo que colocamos en esa variable
+
+def main():
+  print("Hello Winsonsin")
+
+main()
