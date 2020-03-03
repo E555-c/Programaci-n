597227bef445da910ba87b358e888fd02a314b42
E555-c <evepacheco419@gmail.com>
2020-03-03 03:29:06 +0000

    Preguntando un dato y verificar si es correcto o no

---
+++ e4.py
@@ -0,0 +1,11 @@
+#En este codigo tambien interactuamos con el usuario ya que tiene que adivinar cual es mi personaje favorito
+#Al codigo le digo cual es mi personaje para que cuando el usuario escriba quien cree que es la computadora pueda decir si lo adivino o no 
+#Utilizamos el if y el else ya que son codicionales, osea si escribio Jughead entonces imprime correcto, sino imprime incorrecto 
+
+print("Adivina el personaje favorito de Evelyn")
+print("RIVERDALE")
+p1 = input("¿Cual crees que sea su personaje favorito de Riverdale?: ")
+if (p1 == Jughead):
+    print("CORRECTO")
+else:
+    print("INCORRECTO")
