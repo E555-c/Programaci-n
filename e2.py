b28998588d1735f05b941902607115d06736c325
E555-c <evepacheco419@gmail.com>
2020-03-03 02:43:34 +0000

    mandar a llamar con una variable

---
+++ e2.py
@@ -0,0 +1,7 @@
+#Ahora se define la misma oracion, pero utilizando una variable
+#En la variable se pone la frase que se desea imprimir, y al final del codigo dices lo que quieres hacer con esa variable, en este caso la imprimimos
+
+
+variable = "Hello Winsconsin, usando una variable"
+print(variable)
+
